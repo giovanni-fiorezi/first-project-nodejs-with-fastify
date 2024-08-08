4c1c7de60191045152497194266608130f6d70ee
giovanni-fiorezi <giovannifgiovanelli@gmail.com>
2024-08-08 19:16:59 -0300

    finalizando a implementação das rotas, com os devidos métodos

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,3 +2,14 @@
 
 import { Knex } from 'knex';
 
+declare module 'knex/types/tables' {
+    export interface Tables {
+        transactions: {
+            id: string
+            title: string
+            amount: number
+            created_At: string
+            session_id?: string //significa que é opcional
+        }
+    }
+}
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,7 +4,31 @@ import { knex } from "../database";
 import crypto from 'node:crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-   
+
+    app.get('/', async () => {
+        const transactions = await knex('transactions').select();
+        return { transactions };
+    });
+
+    app.get('/:id', async(request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionParamsSchema.parse(request.params);
+
+        const transaction = await knex('transactions').select('id', id).first();
+        return { transaction };
+    });
+
+    app.get('/summary', async() => {
+        const transaction = await knex('transactions')
+        .sum('amount', { as: 'amount' })  // colocar o nome da coluna na função
+        .first();
+
+        return { transaction };
+    })
+
     app.post('/', async (request, response) => {
         const createTransactionBodySchema = z.object({
             title: z.string(),
